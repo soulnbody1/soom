3e33c371a4e3038438c57cd855ff08e1c3ff668e
hana_milad <89672587+hanamilad@users.noreply.github.com>
2025-10-09 12:24:28 +0300

    name in categories is nullable

--- app/Http/Requests/Category/UpdateCategoryRequest.php
+++ app/Http/Requests/Category/UpdateCategoryRequest.php
@@ -15,7 +15,7 @@ class UpdateCategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required', 'string', 'max:255', Rule::unique('categories')->ignore($this->route('category')->id)],
+            'name' => ['nullable', 'string', 'max:255', Rule::unique('categories')->ignore($this->route('category')->id)],
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:3072',
             'display_order' => 'nullable|integer',
             'parent_id' => 'nullable|exists:categories,id|not_in:' . $this->route('category')->id,
