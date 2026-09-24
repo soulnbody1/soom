4f4c4e9a8a3048f2d9dca66046359b0cf824ce25
Hanna Milad <hanamilad9@gmail.com>
2026-09-24 22:11:11 +0300

    Defer trusted host config until application boot

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -33,15 +33,17 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $rootDomainPattern = preg_quote(config('markets.root_domain'), '/');
+        $middleware->trustHosts(at: function (): array {
+            $rootDomainPattern = preg_quote(config('markets.root_domain'), '/');
 
-        $middleware->trustHosts(at: fn (): array => array_values(array_filter([
-            '^'.$rootDomainPattern.'$',
-            '^[a-z]{2}\\.'.$rootDomainPattern.'$',
-            '^api-[a-z]{2}\\.'.preg_quote(config('markets.root_domain'), '/').'$',
-            '^'.preg_quote(config('markets.admin_api_host'), '/').'$',
-            config('markets.legacy_api_host') ? '^'.preg_quote((string) config('markets.legacy_api_host'), '/').'$' : null,
-        ])), subdomains: false);
+            return array_values(array_filter([
+                '^'.$rootDomainPattern.'$',
+                '^[a-z]{2}\\.'.$rootDomainPattern.'$',
+                '^api-[a-z]{2}\\.'.$rootDomainPattern.'$',
+                '^'.preg_quote(config('markets.admin_api_host'), '/').'$',
+                config('markets.legacy_api_host') ? '^'.preg_quote((string) config('markets.legacy_api_host'), '/').'$' : null,
+            ]));
+        }, subdomains: false);
 
         app('router')->aliasMiddleware('role', RoleMiddleware::class);
         app('router')->aliasMiddleware('api_maintenance', ApiMaintenanceMode::class);
